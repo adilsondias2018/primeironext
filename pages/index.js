7c627c8ca3c0a9d50c4d1307d9fc8b30f7f7d90d
Adilson Dias <adilsondias@outlook.com>
2021-05-29 13:11:49 -0300

    chore: arrumando erro botao3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import Btn from "../components/Button";
+import Btn from "../components/Button";
 
 function Home() {
   const [counter, setCounter] = useState(1);
@@ -17,7 +17,7 @@ function Home() {
       </button>
 
       <div>açlsdkjf alksdjfçalksdjf açlksdjfç aslkdfjaçlsdjfaçlkj</div>
-      {/* <Btn>Botão</Btn> */}
+      <Btn>Botão</Btn>
 
       <ul>
         <li>
